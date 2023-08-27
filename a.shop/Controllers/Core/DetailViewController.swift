647a45605619c96e8d735e6474eb1c159777d2b4
Ilfat <salakhoff.ilfat@gmail.com>
2023-08-27 23:46:40 +0300

    Первая настройка

---
+++ a.shop/Controllers/Core/DetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewController.swift
+//  a.shop
+//
+//  Created by Ильфат Салахов on 27.08.2023.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+    
+}
---
+++ a.shop/Models/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Product.swift
+//  a.shop
+//
+//  Created by Ильфат Салахов on 27.08.2023.
+//
+
+import Foundation
